0afa3dc4d1cbbdb45c934ea5e9f9d21422b7c55b
username <useremail@gmail.com>
2021-11-28 11:06:27 +0700

    ngepush pertama kali

---
+++ index.js
@@ -0,0 +1,6 @@
+const nama = "ihsan maulana mayadi";
+let hobi = "pubg";
+hobi = "jajan";
+//consol data, tracing untuk cari tau suatu hal dalam perkodingan
+console.log(`${nama}`, "suka main"`${hobi}`);
+//extension pretier digunakan untuk merapihkan kodingan ketika kodingan acak acakan dengan cara ctrl+shift+P ketik format pilih pretier
